53b907297a25a879efa168ae0a759bb513cd8f07
chelseymachin <chelseymachin@gmail.com>
2023-04-26 13:43:28 -0700

    demos view template desktop view done; sample demo lines added

--- src/views/DemosView.vue
+++ src/views/DemosView.vue
@@ -1,8 +1,61 @@
 <template>
-    <div class="demos">
-      <h1 class="text-center pt-12">Demos</h1>
+  <div class="demos">
+    <h1 class="text-center pt-12">Demos</h1>
+    <div class="content flex-column">
+      <div class="audio-wrapper">
+        <span class="pb-2">Demo 1:</span>
+        <audio
+          controls
+          src="/sample-sound.mp3"
+          class="pb-4"
+        >
+          <a href="/sample-sound.mp3">
+            Download audio
+          </a>
+        </audio>
+      </div>
+      
+      <div class="audio-wrapper">
+        <span class="pb-2">Demo 2:</span>
+        <audio
+          controls
+          src="/sample-sound.mp3"
+          class="pb-4"
+        >
+          <a href="/sample-sound.mp3">
+            Download audio
+          </a>
+        </audio>
+      </div>
+
+      <div class="audio-wrapper">
+        <span class="pb-2">Demo 3:</span>
+        <audio
+          controls
+          src="/sample-sound.mp3"
+          class="pb-4"
+        >
+          <a href="/sample-sound.mp3">
+            Download audio
+          </a>
+        </audio>
+      </div>
+
+      <div class="audio-wrapper">
+        <span class="pb-2">Demo 4:</span>
+        <audio
+          controls
+          src="/sample-sound.mp3"
+          class="pb-4"
+        >
+          <a href="/sample-sound.mp3">
+            Download audio
+          </a>
+        </audio>
+      </div>
     </div>
-  </template>
+  </div>
+</template>
   
   <style scoped>
   @media only screen and (max-width: 600px) {
@@ -12,6 +65,16 @@
   h1 {
     font-size: 3rem;
     font-weight: bold;
+  }
+
+  .audio-wrapper {
+    margin: 0 auto;
+    width: 80%;
+  }
+
+  audio {
+    display: inline;
+    padding-left: 1rem;
   }
   
   </style>
