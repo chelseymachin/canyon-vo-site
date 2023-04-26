fba789d122580f53ed282cc8d51167efa680037e
chelseymachin <chelseymachin@gmail.com>
2023-04-26 12:53:46 -0700

    Add template flesh out for About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="about">
     <h1 class="text-center pt-12">About</h1>
+    <div class="content">
+      <img class="avatar p-10" src="../assets/chris-avatar.png" />
+      <p class="p-8">
+        Bocconcini roquefort halloumi. Fromage frais danish fontina bavarian bergkase cheese triangles taleggio port-salut cheese on toast cheese on toast. Fromage frais who moved my cheese taleggio cheesy feet parmesan cheese triangles fondue jarlsberg. Fondue everyone loves when the cheese comes out everybody's happy stilton smelly cheese cheesecake.
+
+        Cheese and biscuits when the cheese comes out everybody's happy mascarpone. Cream cheese squirty cheese emmental boursin cheeseburger gouda chalk and cheese croque monsieur. Mozzarella when the cheese comes out everybody's happy mascarpone port-salut cheese and biscuits lancashire bavarian bergkase halloumi. Pepper jack pecorino parmesan brie taleggio.
+
+        Manchego red leicester fromage. Halloumi cut the cheese boursin swiss babybel cheeseburger gouda cheese strings. Cheesecake port-salut swiss fondue boursin fromage frais cheese on toast when the cheese comes out everybody's happy. Pecorino hard cheese pepper jack.
+
+        Rubber cheese st. agur blue cheese dolcelatte. Cottage cheese fondue fondue macaroni cheese pecorino the big cheese cheese and biscuits caerphilly. Melted cheese cheese on toast cheddar parmesan taleggio danish fontina cheese strings smelly cheese. Cheese triangles say cheese babybel fromage cut the cheese bocconcini st. agur blue cheese roquefort. Dolcelatte st. agur blue cheese bavarian bergkase.
+
+        Cow croque monsieur cheesy feet. Cheesy grin brie cheddar cottage cheese cheese on toast cheese and wine paneer danish fontina. Chalk and cheese jarlsberg rubber cheese bavarian bergkase hard cheese cut the cheese fondue hard cheese. Red leicester swiss.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -12,6 +26,17 @@
 h1 {
   font-size: 3rem;
   font-weight: bold;
+}
+
+.avatar {
+  border-radius: 55%;
+  margin: 0 auto;
+}
+
+p {
+  background-color: var(--bold-coral);
+  color: var(--dark-muted-green);
+  border-radius: 2rem;
 }
 
 </style>
